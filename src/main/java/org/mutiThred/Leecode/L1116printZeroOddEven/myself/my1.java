74b3cdcffbda409aab6c84498bdf1fad4aa04ca5
bolin <1607944055@qq.com>
2024-10-16 10:26:56 +0800

    HHO交替打印

---
+++ src/main/java/org/mutiThred/Leecode/L1116printZeroOddEven/myself/my1.java
@@ -0,0 +1,152 @@
+package org.mutiThred.Leecode.L1116printZeroOddEven.myself;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntConsumer;
+
+public class my1 {
+    volatile boolean ji = true;
+
+    volatile boolean zero = true;
+    private int n;
+
+    AtomicInteger n2 = new AtomicInteger(1);
+
+
+    public my1(int n) {
+        this.n = n;
+
+
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+//        while n>0不能漏掉啊
+        while (n2.get() <= n) {
+            while (!zero) {
+                Thread.yield();
+            }
+            if (n2.get() <= n) {
+//                System.out.print("even" + " " + n2.get() + " " + n);
+//                System.out.println();
+                printNumber.accept(0);
+
+            }
+
+            zero = false;
+
+        }
+
+
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        while (n2.get() <= n) {
+            while (zero || ji) {
+                Thread.yield();
+            }
+            if (n2.get() <= n) {
+//                System.out.print("even" + " " + n2.get() + " " + n);
+//                System.out.println();
+                printNumber.accept(n2.get());
+
+            }
+
+
+            n2.incrementAndGet();
+            zero = true;
+            ji = true;
+
+        }
+
+
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        while (n2.get() <= n) {
+            while (zero || !ji) {
+                Thread.yield();
+            }
+            if (n2.get() <= n) {
+//                System.out.print("even" + " " + n2.get() + " " + n);
+//                System.out.println();
+                printNumber.accept(n2.get());
+
+            }
+
+            n2.incrementAndGet();
+            zero = true;
+            ji = false;
+
+        }
+
+    }
+
+
+    public void printOdd(IntConsumer printNumber) throws InterruptedException {
+        while (n2.get() <= n) {
+            while (zero) {
+                Thread.yield();
+            }
+            printNumber.accept(n2.get());
+            n2.incrementAndGet();
+            zero = true;
+//            ji=false;
+
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        IntConsumer intConsumer = new IntConsumer() {
+            @Override
+            public void accept(int value) {
+                System.out.println(value);
+            }
+        };
+        my1 my1 = new my1(8);
+        Thread thread1 = new Thread(() -> {
+            try {
+                my1.zero(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                my1.even(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        Thread thread3 = new Thread(() -> {
+            try {
+                my1.odd(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        Thread thread4 = new Thread(() -> {
+            try {
+                my1.printOdd(intConsumer);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+//        thread4.start();
+
+        Thread.sleep(10000);
+
+
+    }
+}
---
+++ src/main/java/org/mutiThred/Leecode/L117produceH20/myself/my1.java
@@ -0,0 +1,93 @@
+package org.mutiThred.Leecode.L117produceH20.myself;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class my1 {
+//    注意这个题目其实他是会调用3个线程的，而不是调用两个线程
+    AtomicInteger size2=new AtomicInteger(2);
+    int size=2;
+    public my1() {
+
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        while (size2.get()==0){
+            Thread.yield();
+
+        }
+
+        releaseHydrogen.run();
+        size2.decrementAndGet();
+    }
+//     oxygen
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+       while (size2.get()>0){
+           Thread.yield();
+
+       }
+       releaseOxygen.run();
+
+       size2.getAndAdd(2);
+
+    }
+
+    public static void main(String [] args) throws InterruptedException {
+        my1 my1 = new my1();
+
+        Thread thred1 = new Thread(() -> {
+            try {
+                my1.hydrogen(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("H");
+                    }
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        Thread thred2 = new Thread(() -> {
+            try {
+                my1.oxygen(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("O");
+                    }
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+
+        Thread thred3 = new Thread(() -> {
+            try {
+                my1.hydrogen(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("H");
+                    }
+                });
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+        thred2.start();
+        thred1.start();
+        thred3.start();
+
+
+        Thread.sleep(4000);
+
+    }
+
+}
