0af587d2f2ff528872333d1fca8ff467110414cb
bolin <1607944055@qq.com>
2024-11-06 08:06:10 +0800

    java basic 基本数据类型量级

---
+++ src/main/java/org/bolin/javaBasic/basicDataType/first.java
@@ -0,0 +1,25 @@
+package org.bolin.javaBasic.basicDataType;
+
+
+public class first {
+    public static void main(String[] args){
+//        System.out.println(Integer.MAX_VALUE);
+        float minValue = Float.MIN_VALUE;
+        float maxValue = Float.MAX_VALUE;
+        float f1=987.1234f;
+        float f2=987.12341f;
+        System.out.println(f1+" "+ f2);
+        double d1=997654321.1234567;
+        double d2=987654321.987654321;
+        System.out.println(d1+" "+d2);
+
+//        System.out.println(minValue);
+        char x=9999;
+        System.out.println(x);
+        long l1 = 1234567891011l;
+        float dL1=l1;
+        System.out.println(dL1);
+
+
+    }
+}
