0ac599960f1f8b769bc060f6896bb109f53f2c42
bolin <1607944055@qq.com>
2024-11-07 10:03:28 +0800

    异常学习

---
+++ src/main/java/org/bolin/javaBasic/Exceptio/try1/Keyword.java
@@ -0,0 +1,17 @@
+package org.bolin.javaBasic.Exceptio.try1;
+
+
+
+import java.io.IOError;
+
+public class Keyword {
+    public static void main(String[] args){
+        IOError ioError =null;
+        Error error = new Error();
+        NullPointerException nullPointerException = new NullPointerException();
+        ClassNotFoundException classNotFoundException = new ClassNotFoundException();
+
+
+
+    }
+}
