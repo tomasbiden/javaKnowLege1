3a33e3856f5be10a24008d470fbd001f8810cc7e
bolin <1607944055@qq.com>
2025-03-16 11:52:53 +0800

    二分查找

--- src/main/java/org/bolin/algorithm/Array/group1/dir1/L704erFenChaZhao.java
+++ src/main/java/org/bolin/algorithm/Array/group1/dir1/L704erFenChaZhao.java
@@ -1,4 +1,22 @@
 package org.bolin.algorithm.Array.group1.dir1;
 
 public class L704erFenChaZhao {
+
+
+//    try1 2025 03 15
+    public static int  search(int[] nums,int target){
+        int left=0;
+        int right=nums.length-1;
+        while (left<=right){
+            int middle=(right+left)/2;
+            if(nums[middle]>target){
+                right=middle-1;
+            }else if(nums[middle]<target){
+                left=middle+1;
+            }else {
+                return  middle;
+            }
+        }
+        return  0;
+    }
 }
